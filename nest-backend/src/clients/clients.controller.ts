52046f0a7a9c9f33d74f9f560e03193dfbdd7367
ProgSomel <somelahmed88@gmail.com>
2024-03-08 12:27:37 +0600

    Client login done

--- nest-backend/src/clients/clients.controller.ts
+++ nest-backend/src/clients/clients.controller.ts
@@ -12,6 +12,7 @@ import {
   ValidationPipe,
   UseInterceptors,
   UploadedFile,
+  Query,
   // ParseEnumPipe,
   // Res,
 } from '@nestjs/common';
@@ -62,14 +63,14 @@ export class ClientsController {
 //     return this.clientsService.getClientByIdAndUserName(id, userName);
 //   }
 
-//   //! Client Login
-//   @Get('/clientLogin')
-//   clientLogin(
-//     @Query('email') email: string,
-//     @Query('password') password: string,
-//   ): Client {
-//     return this.clientsService.clientLogin(email, password);
-//   }
+  //! Client Login
+  @Get('/clientLogin')
+  clientLogin(
+    @Query('email') email: string,
+    @Query('password') password: string,
+  ): Client {
+    return this.clientsService.clientLogin(email, password);
+  }
 
 //   //! Client Registration
   @Post('/clientRegistration')
