7983b7eabe70c972bd69319fd46402250375532d
ab-ashik <abdullahashik2001@gmail.com>
2024-03-16 16:03:42 +0600

    "new rotues"

--- nest-backend/src/worker/auth/auth.service.ts
+++ nest-backend/src/worker/auth/auth.service.ts
@@ -2,10 +2,11 @@ import { Injectable } from "@nestjs/common";
 import { WorkersService } from "src/worker/worker.service";
 
 
-// @Injectable()
-// export class AuthService {
-//     constructor (
-//         private workerService: WorkersService,
-//         private jwtService: JwtService
-//     ) {}
-// }
+
+@Injectable()
+export class AuthService {
+    constructor (
+        private workerService: WorkersService,
+        private jwtService: JwtService
+    ) {}
+}
