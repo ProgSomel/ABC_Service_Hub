76ad018f5a7d523724070a8798f84ad3919c5887
ProgSomel <somelahmed88@gmail.com>
2024-03-19 11:35:18 +0600

    Remove Service from Client

--- nest-backend/src/app.module.ts
+++ nest-backend/src/app.module.ts
@@ -7,10 +7,7 @@ import { WorkersModule } from './worker/worker.module';
 import {AuthModule} from './worker/auth/auth.module';
 
 @Module({
-<<<<<<< HEAD
-  imports: [TypeOrmModule.forRoot(typeOrmConfig), ClientsModule, ServiceModule]
-=======
+
   imports: [TypeOrmModule.forRoot(typeOrmConfig), ServiceModule, ClientsModule, AuthModule, WorkersModule]
->>>>>>> f73421d587e40811e931c592735c39a220a28d39
 })
 export class AppModule {}
