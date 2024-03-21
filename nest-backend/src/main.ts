27f0e44a192f1702a96a3b995ce3c1e91917e1c9
Somel Ahmed <somelahmed88@gmail.com>
2024-03-21 16:07:43 +0600

    Update main.ts

--- nest-backend/src/main.ts
+++ nest-backend/src/main.ts
@@ -2,10 +2,25 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import * as path from 'path';
+import * as session from 'express-session';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useStaticAssets(path.join(__dirname, '../uploads')); //to use file publically ... so that we can access from anywhere or from browser
+
+  app.use(
+    session({
+    secret: process.env.JWT_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie:{
+    maxAge: 300000
+    }
+    }),
+    );
 
   await app.listen(3000);
 }
