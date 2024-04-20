b2647c55f810056edabdff975953829fd1c4cabd
ab-ashik <abdullahashik2001@gmail.com>
2024-04-20 16:34:32 +0600

    backend

--- nest-backend/src/config/typeORM.config.ts
+++ nest-backend/src/config/typeORM.config.ts
@@ -2,7 +2,9 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { ClientEntity } from 'src/clients/clients.entity';
 import { ContactInfoEntity } from 'src/clients/contact-info.entity';
 import { OrderEntity } from 'src/order/order.entity';
+import { ReviewEntity } from 'src/review/review.entity';
 import { ServiceEntity } from 'src/service/services.entity';
+import { WorkersEntity } from 'src/worker/worker.entity';
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
@@ -11,7 +13,7 @@ export const typeOrmConfig: TypeOrmModuleOptions = {
   username: 'postgres',
   password: '1234',
   database: 'ABC_Service_Hub', //Change to your database name
-  entities: [ClientEntity, ContactInfoEntity, OrderEntity, ServiceEntity],
+  entities: [ClientEntity, ContactInfoEntity, OrderEntity, ServiceEntity, WorkersEntity,ReviewEntity],
   autoLoadEntities: true,
   synchronize: true,
 };
--- nest-backend/src/worker/dto/worker.dto.ts
+++ nest-backend/src/worker/dto/worker.dto.ts
@@ -95,3 +95,4 @@ export class updateWorkerDTO {
     @IsBoolean()
     availability: boolean;
 }
+
